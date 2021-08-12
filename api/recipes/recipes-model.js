e256f07d71c98cda971ec45d35dec2bb9aca21ce
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-12 14:52:19 -0700

    ran debugger added brain storm for router that is comented out

--- api/recipes/recipes-model.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const server = require('./api/server.js');
-
-const PORT = process.env.PORT 
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+// router.get("/something", async (req,res, next) => {
+    
+// })
+
 router.use('*', (req, res) => {
     res.json({ api: "up"})
 })
